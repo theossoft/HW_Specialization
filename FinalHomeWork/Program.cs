49a52537f5a9fd0da093ed498de865b2bd75b8bb
theos <theossoft@gmail.com>
2023-03-26 13:24:41 +0300

    Задача FinalHomeWork решена

--- FinalHomeWork/Program.cs
+++ FinalHomeWork/Program.cs
@@ -9,3 +9,57 @@
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
+
+string[] a = new string[] { "hello", "2", "world", ":-)" };
+string[] b = new string[] { "1234", "1567", "-2", "computer science" };
+string[] c = new string[] { "Russia", "Denmark", "Kazan" };
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+string[] StringLengthLessFour(string[] arr)
+{
+    int newArrLength = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < arr[i].Length; j++)
+        {
+            count++;
+        }
+        if (count < 4)
+        {
+            newArrLength++;
+        }
+    }
+
+    string[] newArr = new string[newArrLength];
+    int newArrIndex = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < arr[i].Length; j++)
+        {
+            count++;
+        }
+        if (count < 4)
+        {
+            newArr[newArrIndex] = arr[i];
+            newArrIndex++;
+        }
+    }
+    return newArr;
+}
+
+PrintArray(StringLengthLessFour(a));
+PrintArray(StringLengthLessFour(b));
+PrintArray(StringLengthLessFour(c));
+
